c9066ca01ea11e94d4154394557039c4ca9c36d0
rhtonmoy1 <rhtonmoyhd@gmail.com>
2022-11-16 04:36:19 +0600

    API call
    API URL =  https://jsonplaceholder.typicode.com/users
    
    when ever call an API use
    componentDidMount( ){ } function
    
    inside componentDidMount fetch (API url link)
    then convert the response into json .then((
    response) => response.json())
    then call the users  .then((users)=> this.setStage();}

--- src/App.js
+++ src/App.js
@@ -7,30 +7,29 @@ class App extends Component{
   constructor(){
     super();
     this.state = {
-      monster:[
-        {
-          id:'1',
-          name:'Tonmoy'
-        },
-        {
-          id:'2',
-          name:'Foysal'
-        },
-        {
-          id:'3',
-          name:'Rafi'
-        }, 
-        {
-          id:'4',
-          name:'Sayem'
-        },
-      ]
+      monsters:[],
     };
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((Response)=>Response.json())
+    .then((users)=>
+    this.setState(
+      ()=>{
+        return { monsters: users};
+      },
+
+      ()=>{
+        console.log(this.state);
+      }
+    )
+    )
   }
 
     render(){
       return <div className='App'>
-        {this.state.monster.map((monster)=>{
+        {this.state.monsters.map((monster)=>{
           return (
             <div key={monster.id}> 
                <h1>{monster.name}</h1>

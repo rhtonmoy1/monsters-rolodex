94ac01ea38478d62f54ab723f039319d8f21293d
rhtonmoy1 <rhtonmoyhd@gmail.com>
2022-11-16 03:00:57 +0600

    Click object

--- src/App.js
+++ src/App.js
@@ -4,22 +4,27 @@ import './App.css';
 import React from 'react';
 
 class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+      name: {firstName:'Rafiul Hasan', lastName:'Tonmoy'},
+      company: 'V-Generation',
+    }
+  }
+
     render(){
       return (
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
             <p>
-              Hello I am Tonmoy
+              Hello I am {this.state.name.firstName} {this.state.name.lastName}<br></br>
+              I work at {this.state.company}
             </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
+             <button onClick={()=>{
+              this.setState({name:{firstName:'Rakibul Hasan',lastName:'Foysal'}});
+              console.log(this.state);
+             }}>Change Name</button>
           </header>
         </div>
       );

666b50f177284ce1b15f4520ae9c9d5b666347ea
rhtonmoy1 <rhtonmoyhd@gmail.com>
2022-11-17 01:33:32 +0600

    style card-list
    Image source
    https://robohash.org/1?set=set2&size=180x180

--- src/comonents/card-list/card-list.component.jsx
+++ src/comonents/card-list/card-list.component.jsx
@@ -1,15 +1,24 @@
 import { Component } from 'react';
-
+import './card-list.style.css';
 class CardList extends Component {
     render() {
         const { monsters } = this.props;
 
         return (
-            <div>
-                {monsters.map((monster) =>
-                (
-                    <h1 key={monster.id}>{monster.name}</h1>
-                ))}
+            <div className='card-list'>
+                {monsters.map((monster) => {
+                    const{name, email,id}=monster;
+                    return (
+                        <div className='card-container' key={id}>
+                            <img src={`https://robohash.org/${id}?set=set2&size=180x180`} alt={`monster ${name}`} />
+                            <h2>{name}</h2>
+                            <p>{email}</p>
+                        </div>
+
+                    )
+                })}
+
+
             </div>
 
         )

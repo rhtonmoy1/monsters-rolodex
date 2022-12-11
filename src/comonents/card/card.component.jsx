5de7e324e62a1ffec0e28a67f680782bde30b265
Rafiul Hasan Tonmoy <rhtonmoyhd@gmail.com>
2022-12-11 22:10:36 +0600

    Update card.component.jsx

--- src/comonents/card/card.component.jsx
+++ src/comonents/card/card.component.jsx
@@ -8,6 +8,7 @@ class Card extends Component {
             <div className='card-container' key={id}>
                 <img
                     src={`https://robohash.org/${id}?set=set3&size=180x180`}
+                    // src={`https://robohash.org/${id}?set=set3&size=180x180`}
                     alt={`monster ${name}`}
                 />
                 <h2>{name}</h2>
